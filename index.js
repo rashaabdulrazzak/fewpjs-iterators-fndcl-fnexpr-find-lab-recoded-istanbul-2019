390e4715b6eb8536437a3d72a248771bde4bdd56
Rasha Abdulrazzak <rasha.abdulrazzak@gmail.com>
2020-01-31 20:49:50 +0000

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
+function isWinner (obj){
+  return obj.result === "W";
+}
+function superbowlWin(record){
+  if(record.find(isWinner)) {
+    let s = record.find(isWinner);
+    return s.year;
+  }
+  else return undefined;
+}
+superbowlWin(record)
